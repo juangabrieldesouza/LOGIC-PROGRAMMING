7fa67b0b388d484a020ec6bb061aaf0aefb13cd5
Juan Gabriel <jgstudent7@gmail.com>
2024-01-08 04:11:48 +0100

    atualização

--- Pratic Programming Logic/Exercise07/Exercise07/Program.cs
+++ Pratic Programming Logic/Exercise07/Exercise07/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,24 @@ namespace Exercise07
     {
         static void Main(string[] args)
         {
+            
 
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Unidades de medidas tipo DISTANCIA");
+            Console.WriteLine("----------------------------------");
+            Console.Write("Digite uma medida em metros: ");
+            double metros = double.Parse(Console.ReadLine());
+
+            double decimetros = (metros) * 10;
+            double centimetros = ((metros) * 10) * 10;
+            double milimetros = (((metros) * 10) * 10) * 10;
+
+            double decametro = (metros) / 100;
+            double hectometro = ((metros)/10) / 100;
+            double kilometro = (((metros) / 10) / 10)/100;
+
+            Console.WriteLine($"Valor em:\n decametro: {decametro.ToString("F2")}\n hectometro: {hectometro.ToString("F2")}\n kilometro: {kilometro.ToString("F2")}\n decimetros: {decimetros.ToString("F2")}\n centímetros: {centimetros.ToString("F2")}\n milímetros: {milimetros.ToString("F2")}");
+            
         }
     }
 }
---
+++ Pratic Programming Logic/Exercise08/Exercise08/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Conversor de moedas");
+            Console.WriteLine("-------------------");
+
+            Console.Write("Digite quantos reais você têm: ");
+            double real = double.Parse(Console.ReadLine());
+            double dolar = real / 4.8;
+
+            Console.WriteLine("A quantidade de dólares que você pode comprar é: US$ "+dolar.ToString("F2"));
+
+
+
+
+        }
+    }
+}
