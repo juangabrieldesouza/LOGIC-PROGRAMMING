2ed696b6d3edc5273d70b2ba8e0439634398ae70
Juan Gabriel <jgstudent7@gmail.com>
2024-01-09 14:15:02 +0100

    Aula 7 de algoritsmos
    Aula 7 - algoritmo e logica de programação
    
    professor Gustavo Guanabara

---
+++ Pratic Programming Logic/AULAS/Aula07/atividades propostas/AlunoAprovadoOuReprovado/AlunoAprovadoOuReprovado/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlunoAprovadoOuReprovado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------");
+            Console.WriteLine("ESCOLA VITÓRIA RÉGIA");
+            Console.WriteLine("---------------------");
+
+            Console.Write("Primeira Nota: ");
+            double nota1 = double.Parse(Console.ReadLine());
+            Console.Write("Segunda Nota: ");
+            double nota2 = double.Parse(Console.ReadLine());
+
+            double nota = (nota1 + nota2)/2;
+
+            Console.WriteLine("--------------------");
+            Console.WriteLine("MÉDIA: " + nota);
+            
+            if (nota > 6) Console.WriteLine("ALUNO APROVADO");
+            else Console.WriteLine("ALUNO REPROVADO");
+
+
+            Console.WriteLine("--------------------");
+            //   /  \
+        }
+
+    }
+}
---
+++ Pratic Programming Logic/AULAS/Aula07/atividades propostas/EstaAptoADirigir/EstaAptoADirigir/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstaAptoADirigir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------------"); 
+            Console.WriteLine("DEPARTAMENTO DE TRANSITO");
+            Console.WriteLine("-------------------------\n");
+
+            Console.Write("Ano atual (yyyy): ");
+            int anoAtual = int.Parse(Console.ReadLine());
+            Console.Write("Ano de nascimento (yyyy): ");
+            int anoNasc = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            int idade = anoAtual - anoNasc;
+
+            Console.WriteLine("------------STATUS-----------");
+            Console.WriteLine("IDADE: " + idade + " anos");
+            if (idade > 17)
+            {
+                
+                Console.WriteLine("ESTÁ APTO A TIRAR A CARTEIRA");
+            }else Console.WriteLine("NÃO ESTÁ APTO PARA TIRAR A CARTEIRA");
+
+            Console.WriteLine("-----------------------------");
+
+            // /  \
+
+        }
+    }
+}
