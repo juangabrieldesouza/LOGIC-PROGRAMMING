0d5e2236ae972a347e92070241dc21e361e0c9b8
Juan Gabriel <jgstudent7@gmail.com>
2024-01-23 16:05:30 +0100

    update

---
+++ Pratic Programming Logic/EXERCÍCIOS PROPOSTOS/Exercise32/Exercise32/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int numero = random.Next(1,5);
+            Console.Write("Advinhe o número de 1 a 5: ");
+            int escolha = int.Parse(Console.ReadLine());
+
+            if (escolha == numero) 
+            {
+                Console.WriteLine("ACERTOU!! PARABÉNS!!");
+            } else if (escolha != numero && escolha > 1 && escolha <= 5) 
+            {
+                Console.WriteLine("Infelizmente você errou!");
+            } else Console.WriteLine("VALOR INSERIDO INVÁLIDO!");
+
+            if (escolha > 1 && escolha <= 5) Console.WriteLine("Número correto: "+numero);
+        }
+    }
+}
