424e9aa5cbd7a635c2df0ec541a59234c9feec0e
Juan Gabriel <jgstudent7@gmail.com>
2024-02-01 23:12:17 +0100

    update

---
+++ Pratic Programming Logic/EXERCÍCIOS PROPOSTOS/Exercise46/Exercise46/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise46
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int contador = 6;
+            int numero = 0;
+            while (contador <= 100)
+            {
+                Console.WriteLine(contador);
+                contador +=2;
+                numero = numero + contador;
+            }
+
+            Console.WriteLine("SOMA TOTAL: "+numero);
+
+
+
+
+
+        }
+    }
+}
