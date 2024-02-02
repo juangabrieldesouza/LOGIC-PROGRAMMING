2f8bcd6ffea8490cff31e7bb145fd545e3ef2080
Juan Gabriel <jgstudent7@gmail.com>
2024-02-02 23:44:35 +0100

    update

---
+++ Pratic Programming Logic/EXERCÍCIOS PROPOSTOS/Exercise47/Exercise47/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise47
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 500;
+            int soma = 0;
+            while (contador > 0)
+            {
+                Console.Write(" "+contador);
+                soma = soma + contador;
+                contador -=50;
+
+
+            }
+            Console.WriteLine(" "+soma);
+        }
+    }
+}
