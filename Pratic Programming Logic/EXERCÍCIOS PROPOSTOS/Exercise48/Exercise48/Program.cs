b93a6a1b7ef903868a693e99d3c66651f2f76d92
Juan Gabriel <jgstudent7@gmail.com>
2024-02-05 23:26:31 +0100

    Exercicio 48
    while

---
+++ Pratic Programming Logic/EXERCÍCIOS PROPOSTOS/Exercise48/Exercise48/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise48
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int contador = 1;
+            int soma = 0;
+            int junto = 0;
+            while (contador <= 7)
+            {
+                Console.Write("Digite o " + contador + "o. número: ");
+                soma = int.Parse(Console.ReadLine());
+
+                contador++;
+                
+
+                junto += soma;
+
+            }
+
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("A soma total dos números é "+junto);
+        }
+    }
+}
