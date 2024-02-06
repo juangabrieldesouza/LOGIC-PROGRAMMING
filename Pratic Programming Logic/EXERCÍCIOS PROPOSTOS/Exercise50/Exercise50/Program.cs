33914bceaf462b30ed51465cf60571b71a1a3462
Juan Gabriel <jgstudent7@gmail.com>
2024-02-06 09:27:24 +0100

    dia 2 - exercicios

---
+++ Pratic Programming Logic/EXERCÍCIOS PROPOSTOS/Exercise50/Exercise50/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise50
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int contador = 1;
+            Random random = new Random();
+            while (contador <= 20)
+            {
+                random.Next(0,20);
+            }
+
+
+
+        }
+    }
+}
