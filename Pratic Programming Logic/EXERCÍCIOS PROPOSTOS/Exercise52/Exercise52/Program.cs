ff70b5cd273498cb36744a6be2b66499757ff56e
Juan Gabriel <jgstudent7@gmail.com>
2024-02-09 12:14:17 +0100

    dia 4 - update exercicio 52

---
+++ Pratic Programming Logic/EXERCÍCIOS PROPOSTOS/Exercise52/Exercise52/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise52
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 1, menos = 0, maiorIdade = 0, maisDeDezoito = 0, idade = 0, media = 0;
+
+
+            while (cont <= 10)
+            {
+                Console.Write("Digite a "+cont+"o. idade: ");
+                idade = int.Parse(Console.ReadLine());
+
+                if (idade > maiorIdade) maiorIdade = idade;
+
+                if (idade < 5) menos++;
+
+                if (idade > 18) maisDeDezoito++;
+
+                media += idade;
+
+
+
+                cont++;
+            }
+
+            Console.WriteLine("Média de idades do grupo: "+(media/10));
+            Console.WriteLine("Quantidade de pessoas maiores de 18 anos: "+maisDeDezoito);
+            Console.WriteLine("Quantidade de pessoas menor que 5 anos: "+menos);
+            Console.WriteLine("Maior idade inserida: "+maiorIdade);
+
+
+        }
+    }
+}
