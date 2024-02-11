82f31a0214fe10b5641e538a1c3f5d8b23d19a83
Juan Gabriel <jgstudent7@gmail.com>
2024-02-11 23:29:01 +0100

    dia 5- exercicio 56

---
+++ Pratic Programming Logic/EXERCÍCIOS PROPOSTOS/Exercise56/Exercise56/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise56
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            int number2 = 0;
+
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Programa de somátoria de números!");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Digite '1111' para interrromper a soma!");
+            do
+            {
+
+                Console.Write("Digite o numero: ");
+                number2 = int.Parse(Console.ReadLine());
+
+                if (number2 != 1111) number += number2;
+
+            } while (number2 != 1111);
+            
+            Console.WriteLine("Total da soma é "+number);
+        }
+    }
+}
