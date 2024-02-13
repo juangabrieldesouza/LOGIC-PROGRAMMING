38e082e76f9f5dfd6464f524d114167201000c58
Juan Gabriel <jgstudent7@gmail.com>
2024-02-13 23:30:02 +0100

    dia 7 - update exercicio 58

---
+++ Pratic Programming Logic/EXERCÍCIOS PROPOSTOS/Exercise58/Exercise58/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise58
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int alunos = 0, idade = 0, id = 0, contador = 1;
+
+
+            while (idade != 999)
+            {
+                Console.Write("Digite a idade do "+contador+"o. aluno: ");
+                idade = int.Parse(Console.ReadLine());
+                if (idade != 999)
+                {
+                    alunos++;
+                }
+                contador++;
+                id = id + idade; 
+            }
+
+            id = id / alunos;
+
+            Console.WriteLine("Quantidade de alunos: "+alunos);
+            Console.WriteLine("Media de idade da turma: " + id);
+
+
+
+
+        }
+    }
+}
