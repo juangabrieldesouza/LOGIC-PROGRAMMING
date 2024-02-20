a768ac2afa375f41861495b44c4a7b939954640a
Juan Gabriel <jgstudent7@gmail.com>
2024-02-20 20:45:17 +0100

    update

--- Pratic Programming Logic/EXERCÍCIOS PROPOSTOS/Exercise60/Exercise60/Program.cs
+++ Pratic Programming Logic/EXERCÍCIOS PROPOSTOS/Exercise60/Exercise60/Program.cs
@@ -40,6 +40,7 @@ namespace Exercise60
                     if (idade < 18 && sexo == "F") mulheres++;
 
                     if (idade > idade2) idade2 = idade; pessoaMaior = nome;
+
                 }
 
                 cont++;
