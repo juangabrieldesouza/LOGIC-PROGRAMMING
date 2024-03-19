87b7890dfb2835ee58c96ff9bed1d7e5472319ef
Juan Gabriel <jgstudent7@gmail.com>
2024-03-19 14:53:09 +0100

    Exercise66

---
+++ Pratic Programming Logic/EXERCÍCIOS PROPOSTOS/Exercise66/Exercise66/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise66
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+
+            Console.Write("Digite um número: ");
+            numero = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                
+
+                Console.WriteLine($"{numero} x {i} = {(numero*i)}");
+
+
+
+            }
+
+
+        }
+    }
+}
