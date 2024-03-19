9389d70ff1c020ac68c7620f6744cb40e536b233
Juan Gabriel <jgstudent7@gmail.com>
2024-03-19 14:29:28 +0100

    exercise64

---
+++ Pratic Programming Logic/EXERCÍCIOS PROPOSTOS/Exercise64/Exercise64/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise64
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i <= 40; i+=5)
+            {
+
+
+
+
+
+                Console.Write(" "+i);
+
+
+            }
+            Console.WriteLine("   Acabou!");
+
+
+
+
+        }
+    }
+}
