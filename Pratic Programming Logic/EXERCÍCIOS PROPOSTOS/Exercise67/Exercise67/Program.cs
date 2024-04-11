3935b281ec667d90e80e1ff6fdf6453259494064
Juan Gabriel <jgstudent7@gmail.com>
2024-04-11 17:41:27 +0200

    html curso presencial

---
+++ Pratic Programming Logic/EXERCÍCIOS PROPOSTOS/Exercise67/Exercise67/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise67
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; )
+            {
+
+            }
+
+
+
+
+        }
+    }
+}
